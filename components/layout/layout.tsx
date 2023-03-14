8b2c74f2678cc7953a0d124d1363aec51864f62c
Leandro Gastón Varela Vargas <varelavargasleandro@outlook.com.ar>
2023-03-14 05:42:39 -0300

    Movida barra de navegación a componente aparte.

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,34 +1,39 @@
 import useWindowWidth from "@/lib/hooks/useWindowWidth";
 import Link from "next/link";
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import styles from './Layout.module.css';
 import SearchBar from "../search-bar/searchBar";
 import { useRouter } from "next/router";
+import Menu, { IMenuLink } from "../menu/Menu";
+
+const menuLinks : IMenuLink[] = [
+    { name: 'Pokemon', path: '/pokemon' },
+    { name: 'Berries', path: '/berries' },
+    { name: 'Places', path: '/places' },
+    { name: 'Items', path: '/items' }
+]
 
 export default function Layout ( {children} : {children: ReactElement} ) {
     const [showMenu, setShowMenu] = useState( false );
     const [windowWidth] = useWindowWidth();
     const { pathname } = useRouter();
-    console.log(!pathname.match("/"));
-    
 
+    useEffect( () => {
+        console.log(showMenu, (windowWidth <= 1024) && showMenu)
+    }, [showMenu] );
+    
     return (
         <div>
             <header className={ styles.header }>
                 {windowWidth <= 1024 && <button className={ styles.button } onClick={ () => setShowMenu(!showMenu) }>
                     <img className={ styles.buttonIcon } src="/menu_icon.svg" alt="Menu Button" />
                 </button>}
-                <h1 className={ styles.logo }>Pokemon Browser</h1>
+                <Link href={ "/" }>
+                    <h1 className={ styles.logo }>Pokemon Browser</h1>
+                </Link>
                 { !(pathname == "/") && <SearchBar/> }
             </header>
-            <nav className={ styles.nav } >
-                <ul className={ styles.linksList } >
-                    <li className={ styles.linksItem }><Link className={ styles.links } href={"/pokemon"}>Pokemons</Link></li>
-                    <li className={ styles.linksItem }><Link className={ styles.links } href={""}>Berries</Link></li>
-                    <li className={ styles.linksItem }><Link className={ styles.links } href={""}>Places</Link></li>
-                    <li className={ styles.linksItem }><Link className={ styles.links } href={""}>Items</Link></li>
-                </ul>
-            </nav>
+            { (windowWidth > 1024 || showMenu) && <Menu menuLinks={ menuLinks } /> }
             <div className={styles.realBody}>{children}</div>
         </div>
     )
---
+++ components/menu/Menu.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import styles from './Menu.module.css';
+
+export interface IMenuLink {
+    name: string,
+    path: string
+}
+
+export default function Menu ( { menuLinks } : { menuLinks : IMenuLink[] } ) {
+    
+    return (
+        <nav className={ styles.nav } >
+            <ul className={ styles.linksList } >
+                { menuLinks.map( (menuLink) => <li className={ styles.linksItem }><Link className={ styles.links } href={ menuLink.path }>{menuLink.name}</Link></li> ) }                
+            </ul>
+        </nav>
+    )
+
+}
