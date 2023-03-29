3921a931004e281278aeb5eafbff44cb616f643e
Leandro Gastón Varela Vargas <varelavargasleandro@outlook.com.ar>
2023-03-29 15:46:43 -0300

    Agregado nuevo hook para buscar varios pokemons.

--- lib/hooks/useSearchPokemon.ts
+++ lib/hooks/useSearchPokemon.ts
@@ -1,24 +1,20 @@
 import { IPokemonCardProp } from '@/components/pokemon-card/PokemonCard';
 import { LIMIT_PAGES, PAGE_LIMIT, POKEMON_SEARCH_URL } from '@/constants/constants';
 import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 
 export interface ISearchPokemon {
-    pokemonName? : string,
-    offset? : number
+    pokemonName? : string
 }
 
-export interface IUseSearchPokemonFn<Type> {
-    (arg : ISearchPokemon): IUseSearchPokemonReturn<Type>
-}
-
-interface IUseSearchPokemonReturn<Type> {
-    pokemons: Type,
+export interface IUseSearchPokemonReturn<Type> {
+    pokemon: Type,
     error: any,
     isLoading: any,
     next: any
 }
 
-const getPokemonURLSearch = ( pokemonName? : string, offset = 0 ) => {
+const getPokemonURLSearch = ( {pokemonName = "", offset = 0} ) => {
     if (!pokemonName) return `${POKEMON_SEARCH_URL}/${LIMIT_PAGES( offset, PAGE_LIMIT )}`;
 
     return `${POKEMON_SEARCH_URL}/${pokemonName}`;
@@ -26,12 +22,29 @@ const getPokemonURLSearch = ( pokemonName? : string, offset = 0 ) => {
 
 const fetcher = (url : string) => fetch(url).then(r => r.json());
 
-export function useSearchPokemon ( { pokemonName, offset } : ISearchPokemon ) {
-    const { data, error, isLoading } = useSWR( getPokemonURLSearch( pokemonName, offset ), fetcher );
+export function useSearchPokemon ( { pokemonName } : ISearchPokemon ) {
+    const { data, error, isLoading } = useSWR( getPokemonURLSearch( {pokemonName} ), fetcher );
 
     return {
-        pokemons: data,
-        next: data?.next,
+        pokemon: data,
+        error,
+        isLoading
+    }
+}
+
+function getKey (pageIndex : number, previousPageData : any) {
+    const url = getPokemonURLSearch( {offset: pageIndex * PAGE_LIMIT} );
+    
+    return url;
+}
+
+export function useSearchPokemons () {
+    const { data, size, setSize, error, isLoading } = useSWRInfinite( getKey, fetcher );
+
+    return {
+        data: data,
+        size,
+        setSize,
         error,
         isLoading
     }
